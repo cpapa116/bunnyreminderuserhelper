0d7534db3c37416a90c3a61aa56ce6c7705e8b75
HSchiff <hschiffty@gmail.com>
2024-11-09 19:48:01 -0500

    Revert "merge"
    This reverts commit de351144857a482ec126a14cba5e9f1abbeefb32, reversing
    changes made to a43665218820cd0a248a6d73fecd4b6267503302.

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,66 +1,23 @@
-const { app, BrowserWindow, Notification, ipcMain } = require('electron');
-const player = require('play-sound')();
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const db = require("./database"); //create database from database.js
 const { ipcMain } = require('electron');
 
 function createWindow() {
+    // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            sandbox: false,
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true,
+            contextIsolation: false
         }
     });
 
-    // Set CSP headers
-    mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-        callback({
-            responseHeaders: {
-                ...details.responseHeaders,
-                'Content-Security-Policy': [
-                    "default-src 'self'; connect-src 'self' https://generativelanguage.googleapis.com; script-src 'self' 'unsafe-eval'"
-                ]
-            }
-        });
-    });
-
+    // Load the index.html file
     mainWindow.loadFile('index.html');
 }
 
-// Listen for the 'show-notification' event from the renderer
-ipcMain.on('show-notification', (event, title, body) => {
-    const notification = new Notification({
-        title: title,
-        body: body
-    });
-
-    const audioPath = path.resolve(__dirname, 'src', 'sounds', 'surprise.mp3');
-
-    // Play sound when notification is displayed
-    player.play(audioPath, function(err) {
-        if (err) {
-          console.error('Error playing audio:', err);
-        } else {
-          console.log('Audio played successfully');
-        }
-    });
-    
-    notification.show();
-});
-
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
